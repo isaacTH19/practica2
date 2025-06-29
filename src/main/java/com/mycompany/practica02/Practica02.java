111bf88327371e752eee0afb49ac809ee2db82f0
isaac <isaac@DESKTOP-EU3I484>
2025-06-28 22:02:33 -0600

    PRUEBA PRACTICA 2

--- src/main/java/com/mycompany/practica02/Practica02.java
+++ src/main/java/com/mycompany/practica02/Practica02.java
@@ -11,6 +11,6 @@ package com.mycompany.practica02;
 public class Practica02 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("PRUEBA GITHUB");
     }
 }
