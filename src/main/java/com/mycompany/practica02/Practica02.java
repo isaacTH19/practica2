590389965dc2f54aca9b50f44366ea4468d48483
isaac <isaac@DESKTOP-EU3I484>
2025-06-28 21:17:55 -0600

    INICIO PRACTICA 2

---
+++ src/main/java/com/mycompany/practica02/Practica02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica02;
+
+/**
+ *
+ * @author isaac
+ */
+public class Practica02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
